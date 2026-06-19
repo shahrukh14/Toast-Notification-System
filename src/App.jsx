a2c5095726d8910f4a95faefb88246d6aa2f3364
Mohammed Shahrukh <mohammedshahrukhalam@gmail.com>
2026-06-19 14:51:53 +0530

    Initial Project Setup and Packages installed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App(){
+  return (
+    <main style={{
+      padding:"40px",
+    }}>
+      <h1>
+        Toast Notification System
+      </h1>
+
+      <p>
+        Enterprise React + Vite Project
+      </p>
+    </main>
+  );
+}
+
+export default App;
